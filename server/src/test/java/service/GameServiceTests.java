4cfe2206493bdcdaa63ddfb9d674e65779149b56
fourh <fourh027@gmail.com>
2026-05-20 13:26:55 -0600

    impl p(3) hypothetically finished gameServiceTests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -24,62 +24,35 @@ public class GameServiceTests {
         UserDAO userDAO = new UserDAO();
         gameService = new GameService(authDAO, gameDAO, userDAO);
         authDAO.createAuth(new AuthData("banana", "Monkey"));
-    }
-    public GameService.CreateGameResult CreateGame(GameService.CreateGameRequest createGameRequest) {
-        AuthData existingAuth = auths.getAuth(createGameRequest.authToken());
-        if (existingAuth == null) {
-            throw new IllegalArgumentException("error null");
-        }
-        GameData gameData = new GameData(0,null,null, createGameRequest.gameName(), new ChessGame());
-        int gameID = games.createGame(gameData);
-        return new GameService.CreateGameResult(gameID);
-    }
-    public boolean checkColorAvailability(GameData gameData, String playerColor) {
-        if (gameData.blackUsername() == null && Objects.equals(playerColor, "BLACK") || gameData.whiteUsername() == null && Objects.equals(playerColor, "WHITE")) {
-            return true;
-        }
-        return false;
-    }
-
-    public void JoinGame(GameService.JoinGameRequest joinGameRequest) {
-        model.GameData gameData = games.getGame(joinGameRequest.gameID());
-        AuthData existingAuth = auths.getAuth(joinGameRequest.authToken());
-        if (existingAuth == null) {
-            throw new IllegalStateException("error null auth");
-        }
-        if (gameData == null) {
-            throw new IllegalArgumentException("error null game");
-        }
-        if (!Objects.equals(joinGameRequest.playerColor, "WHITE") && !Objects.equals(joinGameRequest.playerColor, "BLACK")) {
-            throw new IllegalCallerException("error unauthorized (color not available)");
-        }
-        if (checkColorAvailability(gameData, joinGameRequest.playerColor)) {
-
-            if (Objects.equals(joinGameRequest.playerColor, "BLACK")) {
-                gameData = new GameData(gameData.gameID(), gameData.whiteUsername(),existingAuth.username(),gameData.gameName(),gameData.game());
-            }
-            if (Objects.equals(joinGameRequest.playerColor, "WHITE")) {
-                gameData = new GameData(gameData.gameID(), existingAuth.username(),gameData.blackUsername(),gameData.gameName(),gameData.game());
-            }
-            games.updateGame(gameData);
-        }
-        else {
-            throw new IllegalAccessError("error unauthorized (color not available)");
-        }
+        authDAO.createAuth(new AuthData("apple", "Horse"));
 
     }
-    public GameService.ListGamesResult listGames(String authToken) {
-        if (auths.getAuth(authToken) == null) {
-            throw new IllegalArgumentException("error unauthorized (color not available)");
-        }
-        return new GameService.ListGamesResult(games.listGames());
+    @Test
+    void testCreateGame() {
+        //passes
+        gameService.CreateGame(new GameService.CreateGameRequest("banana", "MonkeyWorld"));
+        assertEquals(1, gameService.listGames("banana").games().size());
+        //fails if auth is wrong
+        assertThrows(IllegalArgumentException.class, () -> {gameService.listGames("carrot");});
+    }
+
+    @Test
+    void testJoinGame() {
+        //passes
+        gameService.CreateGame(new GameService.CreateGameRequest("banana", "MonkeyWorld"));
+        gameService.JoinGame(new GameService.JoinGameRequest("WHITE", 123, "banana"));
+        //fails if auth is wrong
+        assertThrows(IllegalArgumentException.class, () -> {gameService.JoinGame(new GameService.JoinGameRequest("WHITE", 123, "carrot"));});
+        //fails if color is already taken
+        gameService.JoinGame(new GameService.JoinGameRequest("WHITE", 123, "apple"));
+
     }
     @Test
     void testListGames() {
         //passes
         gameService.CreateGame(new GameService.CreateGameRequest("banana", "MonkeyWorld"));
         assertEquals(1, gameService.listGames("banana").games().size());
-        //fails
+        //fails if auth is wrong
         assertThrows(IllegalArgumentException.class, () -> {gameService.listGames("carrot");});
     }
 
