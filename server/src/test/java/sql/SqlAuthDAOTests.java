d847472e68fd4a8134e21469a81f3c722870f206
fourh <fourh027@gmail.com>
2026-05-27 17:50:40 -0600

    impl p(4) finished auth tests working on usertests

--- server/src/test/java/sql/SqlAuthDAOTests.java
+++ server/src/test/java/sql/SqlAuthDAOTests.java
@@ -1,4 +1,24 @@
 package sql;
 
+import dataaccess.*;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.AuthService;
+
 public class SqlAuthDAOTests {
+
+    private SqlAuthDAO authDAO;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        authDAO = new SqlAuthDAO();
+        authDAO.clearAuthData();
+    }
+    @Test
+    void testClear()  throws DataAccessException {
+        AuthData testAuth = new AuthData("testingToken", "testingname");
+        authDAO.createAuth(testAuth);
+        authDAO.clearAuthData();
+    }
 }
--- server/src/test/java/sql/SqlUserDAOTests.java
+++ server/src/test/java/sql/SqlUserDAOTests.java
@@ -1,4 +1,67 @@
 package sql;
 
+import dataaccess.DataAccessException;
+import dataaccess.SqlAuthDAO;
+import dataaccess.SqlUserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SqlUserDAOTests {
+    private SqlUserDAO userDAO;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        userDAO = new SqlUserDAO();
+        userDAO.clearUserData();
+    }
+    @Test
+    void positiveTestRegister()  throws DataAccessException{
+        //passes
+        String hashedPassword = BCrypt.hashpw("llama", BCrypt.gensalt());
+        UserData testUser = new UserData("testingUsername", hashedPassword, "testingEmail");
+        userDAO.createUser(testUser);
+        userDAO.getUser("testingUsername");
+
+    }
+
+    @Test
+    void positiveTestLogin()  throws DataAccessException{
+        //passes
+        String hashedPassword = BCrypt.hashpw("apple", BCrypt.gensalt());
+        UserData testUser = new UserData("testingUsername2", hashedPassword, "testingEmail");
+        userDAO.createUser(testUser);
+        userDAO.getUser("testingUsername2");
+
+    }
+
+    @Test
+    void negativeTestLogin()  throws DataAccessException{
+        //put in wrong password
+        String hashedPassword = BCrypt.hashpw("apple", BCrypt.gensalt());
+        UserData testUser = new UserData("testingUsername2", hashedPassword, "testingEmail");
+        userDAO.createUser(testUser);
+        assertThrows(IllegalArgumentException.class, () -> {
+                userDAO.getUser("wrongPassword");
+         });
+
+    }
+
+    @Test
+    void negativeTestRegister()  throws DataAccessException{
+        //fails because password is blank
+        assertThrows(IllegalArgumentException.class, () -> {
+            userDAO.createUser(new UserData("Carl", "", "mon@gmail.com"));
+        });
+    }
+    @Test
+    void testClear()  throws DataAccessException {
+        UserData testUser = new UserData("testingUsername", "testingPassword", "testingEmail");
+        userDAO.createUser(testUser);
+        userDAO.clearUserData();
+    }
 }
