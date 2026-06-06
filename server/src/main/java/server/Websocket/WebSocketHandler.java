c68e7476d8d4bd88481615df7609a86697657615
fourh <fourh027@gmail.com>
2026-06-06 14:05:45 -0600

    impl p(6) updating websocket handler to work with pulling username and color and game id out so ic an use it later

--- server/src/main/java/server/Websocket/WebSocketHandler.java
+++ server/src/main/java/server/Websocket/WebSocketHandler.java
@@ -1,5 +1,13 @@
 package server.Websocket;
 import chess.ChessMove;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.GameData;
+import service.AuthService;
+import service.GameService;
+import service.UserService;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 import com.google.gson.Gson;
@@ -12,10 +20,27 @@ import io.javalin.websocket.WsMessageHandler;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+    private final ConcurrentHashMap<Session, playerInfo> connections = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Session> observers = new ConcurrentHashMap<>();
 
-    private final ConnectionManager connections = new ConnectionManager();
+    private GameService gameService;
+    private AuthService authService;
+    private UserService userService;
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private UserDAO userDAO;
+    public WebSocketHandler(GameService gameService, AuthService authService, UserService userService, GameDAO gameDAO, AuthDAO authDAO, UserDAO userDAO) {
+        this.gameService = gameService;
+        this.authService = authService;
+        this.userService = userService;
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -38,6 +63,23 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         }
     }
     private void connect(UserGameCommand action, Session session) {
+        System.out.println("Succesffuly conencted to game through websocket");
+        try {
+            String username = authDAO.getAuth(action.getAuthToken()).username();
+            GameData data = gameDAO.getGame(action.getGameID());
+            if (Objects.equals(data.whiteUsername(), username)) {
+                connections.put(session, new playerInfo(true, action.getGameID()));
+            }
+            else {
+                connections.put(session, new playerInfo(false, action.getGameID()));
+
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return;
+
+
     }
 
     private void makeMove(ChessMove move, Session session) {
@@ -53,5 +95,6 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
     public void handleClose(WsCloseContext ctx) {
         System.out.println("Websocket closed");
     }
-
+    record playerInfo(boolean playerIsWhite, int gameID) {
+    }
 }
